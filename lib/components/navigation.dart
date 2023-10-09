da3dfcf4cd2ec721fcc0c2292ad41c3397fe1454
YumanYummy <ahmadrizki0209@gmail.com>
2023-10-09 22:48:43 +0700

    main : add cart_page, category_page, profile_page and navigation components

---
+++ lib/components/navigation.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class NavigatonBar extends StatefulWidget {
+  @override
+  State<NavigatonBar> createState() => _NavigatonBarState();
+}
+
+class _NavigatonBarState extends State<NavigatonBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/views/screens/cart_page.dart
+++ lib/views/screens/cart_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_key_in_widget_constructors
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -10,7 +10,7 @@ class CartPage extends StatefulWidget {
 class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
