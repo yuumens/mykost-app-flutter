559e8f055cd09f94158ee66fca3dd906d1f8201d
YumanYummy <ahmadrizki0209@gmail.com>
2023-10-15 22:36:48 +0700

    main : Extract widget at home_card

--- lib/views/widget/home_card.dart
+++ lib/views/widget/home_card.dart
@@ -37,134 +37,148 @@ class _HomeCardState extends State<HomeCard> {
       scrollDirection: Axis.horizontal,
       child: Padding(
         padding: EdgeInsets.only(bottom: 3.0),
-        child: Row(
-          children: items.map((item) {
-            return Container(
-              padding: EdgeInsets.symmetric(horizontal: 10),
-              width: 300,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(10),
-                        topRight: Radius.circular(10),
-                      ),
-                      image: DecorationImage(
-                        image: AssetImage(item['imagePath']),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    width: double.infinity,
-                    height: 120,
-                    child: Stack(
-                      children: [
-                        Positioned(
-                          top: 5,
-                          right: 5,
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: Color(0xFF4E4B4B),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            padding: EdgeInsets.all(5),
-                            child: Icon(
-                              IconlyBold.heart,
-                              color: Colors.white,
-                              size: 20.0,
-                            ),
-                          ),
-                        ),
-                        Positioned(
-                          top: 40,
-                          right: 5,
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: Color(0xFF4E4B4B),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            padding: EdgeInsets.all(5),
-                            child: Icon(
-                              Icons.share,
-                              color: Colors.white,
-                              size: 20.0,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    width: 300,
-                    height: 45,
-                    padding: EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(10),
-                        bottomRight: Radius.circular(10),
-                      ),
-                      color: Color.fromARGB(255, 249, 228,
-                          192), // Ganti dengan warna yang Anda inginkan
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              item['title'],
-                              style: TextStyle(
-                                fontFamily: 'Plus Jakarta Sans',
-                                fontSize: 10,
-                                fontWeight: FontWeight.w700,
-                              ),
-                            ),
-                            Text(
-                              item['price'],
-                              style: TextStyle(
-                                fontFamily: 'Plus Jakarta Sans',
-                                fontSize: 10,
-                                fontWeight: FontWeight.w700,
-                              ),
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 2,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              item['address'],
-                              style: TextStyle(
-                                fontFamily: 'Plus Jakarta Sans',
-                                fontSize: 10,
-                                fontWeight: FontWeight.w400,
-                              ),
-                            ),
-                            Text(
-                              'per Month',
-                              textAlign: TextAlign.start,
-                              style: TextStyle(
-                                fontFamily: 'Plus Jakarta Sans',
-                                fontSize: 10,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            );
-          }).toList(),
-        ),
+        child: HomeCards(items: items),
       ),
+    );
+  }
+}
+
+class HomeCards extends StatelessWidget {
+  const HomeCards({
+    super.key,
+    required this.items,
+  });
+
+  final List<Map<String, dynamic>> items;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: items.map((item) {
+        return Container(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          width: 300,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10),
+                  ),
+                  image: DecorationImage(
+                    image: AssetImage(item['imagePath']),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                width: double.infinity,
+                height: 120,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      top: 5,
+                      right: 5,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Color(0xFF4E4B4B),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        padding: EdgeInsets.all(5),
+                        child: Icon(
+                          IconlyBold.heart,
+                          color: Colors.white,
+                          size: 20.0,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: 40,
+                      right: 5,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Color(0xFF4E4B4B),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        padding: EdgeInsets.all(5),
+                        child: Icon(
+                          Icons.share,
+                          color: Colors.white,
+                          size: 20.0,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: 300,
+                height: 45,
+                padding: EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(10),
+                    bottomRight: Radius.circular(10),
+                  ),
+                  color: Color.fromARGB(255, 249, 228,
+                      192), // Ganti dengan warna yang Anda inginkan
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          item['title'],
+                          style: TextStyle(
+                            fontFamily: 'Plus Jakarta Sans',
+                            fontSize: 10,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        Text(
+                          item['price'],
+                          style: TextStyle(
+                            fontFamily: 'Plus Jakarta Sans',
+                            fontSize: 10,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 2,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          item['address'],
+                          style: TextStyle(
+                            fontFamily: 'Plus Jakarta Sans',
+                            fontSize: 10,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        Text(
+                          'per Month',
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                            fontFamily: 'Plus Jakarta Sans',
+                            fontSize: 10,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      }).toList(),
     );
   }
 }
